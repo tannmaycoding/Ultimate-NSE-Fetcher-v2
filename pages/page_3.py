22918485be404ab98026a3ec500a8e60c7fc7fb0
Tannmay Khandelwal <94522751+tannmaycoding@users.noreply.github.com>
2024-09-19 19:48:46 +0530

    Update page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -1,19 +1,17 @@
 from nselib import capital_market
 import streamlit as st
 import pandas as pd
+from io import BytesIO
 
 st.title("Stock Market Historical Prices")
 options = ["NSE Share", "Nifty 500", "Nifty 100", "Nifty 200", "Nifty 50", "Nifty Next 50", "Nifty Midcap 150", "Nifty Midcap 50",
            "Nifty Midcap Select", "Nifty Midcap 100", "Nifty Smallcap 250", "India Vix"]
-name = st.selectbox("Broad Market Index Name", options=options)
+name = st.selectbox("NSE Type Name", options=options)
+
 if name == "NSE Share":
     name2 = st.text_input("NSE Share: ")
 
 start, end = st.columns(2, vertical_alignment="top")
-
-# Initialize services_count in session state if it doesn't exist
-if 'services_count' not in st.session_state:
-    st.session_state.services_count = 0
 
 sdate = start.selectbox("Start Date", options=["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12",
                                                "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
@@ -42,12 +40,6 @@ end_date = f"{edate}-{emonth}-{eyear}"
 
 
 def fetch_prices():
-    st.session_state.services_count += 1
-
-    if st.session_state.services_count >= 20:
-        st.error("You have used our services more than 20 times, which is the guest account limit.")
-        st.stop()
-
     try:
         # Fetch data from the library
         if name == "India Vix":
@@ -68,8 +60,18 @@ def fetch_prices():
 
 
 # Function to convert DataFrame to CSV
-def convert_df(dataframe: pd.DataFrame):
+def convert_df_to_csv(dataframe: pd.DataFrame):
     return dataframe.to_csv().encode("utf-8")
+
+
+def convert_df_to_excel(dataframe: pd.DataFrame):
+    output = BytesIO()
+    
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        dataframe.to_excel(writer, index=False, sheet_name='Sheet1')
+    processed_data = output.getvalue()
+    
+    return processed_data
 
 
 plot_col1, plot_col2, plot_col3, plot_col4 = st.columns(4, vertical_alignment="top")
@@ -79,6 +81,7 @@ high = plot_col2.checkbox("High")
 low = plot_col3.checkbox("Low")
 close = plot_col4.checkbox("Close")
 plot_options = []
+
 # Button to fetch graph data
 if st.button("Get Graph"):
     data = fetch_prices()
@@ -107,7 +110,6 @@ if st.button("Get Graph"):
                 st.warning("None of the selected columns are available in the data.")
 
         else:
-
             if openPrice:
                 plot_options.append("OpenPrice")
 
@@ -133,11 +135,20 @@ if start_date != end_date:
         data = fetch_prices()
 
         if data is not None:
-            csv_data = convert_df(data)
-            st.success("Everything is done")
+            # Download as CSV
+            csv_data = convert_df_to_csv(data)
             st.download_button(
-                label="Click here to download",
+                label="Download as CSV",
                 data=csv_data,
-                file_name=f"{name}.csv",
+                file_name=f"{name2}.csv",
                 mime="text/csv"
+            )
+
+            # Download as Excel
+            excel_data = convert_df_to_excel(data)
+            st.download_button(
+                label="Download as Excel",
+                data=excel_data,
+                file_name=f"{name2}.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
